b9a58e6b8ad0fe24c3c7e0136a1f6f968a1c56d7
adi <adikakaam@gmail.com>
2023-12-28 08:46:41 +0530

    week 1 notes added

--- ClassCode&Problems/w1/class1_2.js
+++ ClassCode&Problems/w1/class1_2.js
@@ -223,7 +223,30 @@ console.log("After JSON.stringify():", JSON.stringify(sampleJSONString));
 
 //DATE
 console.log("---------------------------------------");
+console.log("DATE");
+const currentDate = new Date();
+console.log("Current Date: " + currentDate);
 
+// Getting various components of the date
+console.log("Date:", currentDate.getDate());
+console.log("Month:", currentDate.getMonth() + 1); // Months are zero-indexed, so adding 1
+console.log("Year:", currentDate.getFullYear());
+console.log("Hours:", currentDate.getHours());
+console.log("Minutes:", currentDate.getMinutes());
+console.log("Seconds:", currentDate.getSeconds());
+
+// Setting components of the date
+currentDate.setFullYear(2022);
+console.log("Setting current year as 2022:", currentDate);
+
+currentDate.setMonth(5); // Setting month to June (zero-indexed)
+console.log("Setting current month as 5:", currentDate);
+
+// Getting and setting time in milliseconds since 1970
+console.log("Time in milliseconds since 1970:", currentDate.getTime());
+
+const newDate = new Date(2023, 8, 15); // Creating a new date
+console.log("New Date:", newDate);
 
 // CLASS PROBLEMS
 // Write a program to print the biggest number in an arrya
--- ClassNotes/01_2_JAVASCRIPT_BASICS.md
+++ ClassNotes/01_2_JAVASCRIPT_BASICS.md
@@ -38,15 +38,4 @@ Read up on 'Cluster Module'
 |:----------:|:-----------:|
 |All code runs line by line. Next line is not started till current line execution is not finished.|Asynchoronous functions can be used to start long running operations while other parts of the code are also running without waiting for the long running process to complete|
 
-Let’s write some code -
-1. Write a program prints all the even numbers in an array
-2. Write a program to print the biggest number in an arrya
-3. Write a program that prints all the male people’s first name given a complex object
-4. Write a program that reverses all the elements of an array
-
-Assignments
- Create a counter in Javascript (counts down from 30 to 0)
-2. Calculate the time it takes between a setTimeout call and the inner function actually running
-3. Create a terminal clock (HH:MM:SS)
-
 ## Event loops, callback queue, Promises
